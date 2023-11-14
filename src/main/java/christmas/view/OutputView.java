0dbfa3ec6a9b3ca1f4803c1f15dc50a2d1b83ed6
GumaLee <rkdxhd97@naver.com>
2023-11-14 13:04:16 +0900

    Feat(OutputVies): 질의 문구 생성

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,16 @@
+package christmas.view;
+
+public class OutputView {
+
+    public static void printInfoMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printVisitDateQueryMessage() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)");
+    }
+
+    public static void printOrderMenuQueryMessage() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
+}
